a1b15bfb61949da0051687cbd871c7bd1112e0b6
amcourson <amcourson09@sbcglobal.net>
2021-05-08 20:43:49 -0500

    adding mongo driver

--- Develop/server.js
+++ Develop/server.js
@@ -28,6 +28,16 @@ app.listen(PORT, () => {
 });
 
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://budgetpassword:<budgetdbpassword>@cluster0.3huor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 
 
